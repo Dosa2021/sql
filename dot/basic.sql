91f95c18a0464f420af18c1c962bd02922c6e5a5
dosaken <dosa@dosakennoMacBook-Pro.local>
2025-12-11 08:58:00 +0900

    値に制限-13

--- dot/basic.sql
+++ dot/basic.sql
@@ -8,9 +8,10 @@ CREATE TABLE posts
 (
   message VARCHAR(140),
   -- likes INT UNSIGNED NOT NULL,
-  likes INT UNSIGNED DEFAULT 0,
+  -- likes INT UNSIGNED DEFAULT 0,
+  likes INT UNSIGNED CHECK (likes >= 0 AND likes <= 100),
   mood DECIMAL(4, 2) UNSIGNED,
-  lang CHAR(2),
+  lang CHAR(2) UNIQUE,
   category ENUM('Gadget', 'Game', 'Business'),
   categories SET('Gadget', 'Game', 'Business'),
   is_draft BOOL,
@@ -26,7 +27,7 @@ INSERT INTO posts
 INSERT INTO posts
   ( message, likes, mood, lang, category, categories, is_draft, created )
   VALUES
-  ('thanks', 12, 7.825, 'EN', 'Gadget', 'Gadget,Game', TRUE, '2025-12-08 00:00:00'),
+  ('thanks', 100, 7.825, 'EN', 'Gadget', 'Gadget,Game', TRUE, '2025-12-08 00:00:00'),
   ('arigato', 4, 4.21, 'JA', 'Gadget', 3, FALSE, '2025-12-08 00:00:00'),
-  ('arigato', 4, 4.21, 'JA', 'Gadget', 4, 1, '2025-12-08 00:00:00'),
-  ('arigato', 4, 4.21, 'JA', 'Gadget', 5, 0, NOW());
+  ('arigato', 4, 4.21, 'HO', 'Gadget', 4, 1, '2025-12-08 00:00:00'),
+  ('arigato', 4, 4.21, 'FU', 'Gadget', 5, 0, NOW());
